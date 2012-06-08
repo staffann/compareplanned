8be57397a33fdb689ceb1fd264f54563c22c2a12
staffan.e.nilsson@gmail.com <staffan.e.nilsson@gmail.com@269d6aa1-9b62-febe-af87-f584b9918dbc>
2012-06-08 16:43:30 +0000

    Updated Assembly Info

--- ComparePlanned/Properties/AssemblyInfo.cs
+++ ComparePlanned/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CompareView")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ComparePlanned")]
+[assembly: AssemblyDescription("Plugin to Sporttracks that allows planned and performed workouts to be compared")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CompareView")]
+[assembly: AssemblyProduct("ComparePlanned")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0")]
